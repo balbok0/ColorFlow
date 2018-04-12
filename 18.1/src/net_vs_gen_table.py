ea7a9af8ee487185adfc00381b716eb209063ce5
Jakub Filipek <balbok@uw.edu>
2018-04-11 23:13:46 -0700

    Build fully functional net vs gen table.

--- 18.1/src/net_vs_gen_table.py
+++ 18.1/src/net_vs_gen_table.py
@@ -3,10 +3,10 @@ import numpy as np
 import sklearn
 from keras.models import load_model
 from keras.utils import HDF5Matrix
+import matplotlib.pyplot as plt
 
 
 def make_table(f=sklearn.metrics.roc_auc_score):
-    col_rows = generators
     data = []
     i = 0
     for g in generators:
@@ -21,3 +21,24 @@ def make_table(f=sklearn.metrics.roc_auc_score):
             value = f(y_actual, y_pred)
             data[i].append(value)
         i += 1
+
+        # Prepares labels for rows and columns
+        col_labels = generators
+        row_labels = []
+        for i in col_labels:
+            row_labels.append(i + ' net')
+
+        fig, ax = plt.subplots()
+
+        # hide axes
+        fig.patch.set_visible(False)
+        ax.axis('off')
+        ax.axis('tight')
+
+        ax.table(cellText=data, colLabels=col_labels, rowLabels=row_labels, loc='right')
+
+        fig.tight_layout()
+
+        mng = plt.get_current_fig_manager()
+        mng.full_screen_toggle()
+        plt.show()
